203ce7b9cbd5db29fc13d4550f18ce6d2a746e93
Placidina <alanplacidina@gmail.com>
2019-04-01 23:09:32 -0300

    fix(module): No module named coolproxy_handler

--- proxy_handlers/__init__.py
+++ proxy_handlers/__init__.py
@@ -9,7 +9,6 @@ from .gatherproxy_handler import GatherProxyHandler
 from .proxyiplist_handler import ProxyIPListHandler
 from .aliveproxy_handler import AliveProxyHandler
 from .checkerproxy_handler import CheckerProxyHandler
-from .coolproxy_handler import CoolProxyHandler
 from .freeproxylists_handler import FreeProxyListHandler
 from .proxyhttp_handler import ProxyHTTPHandler
 from .proxynova_handler import ProxyNovaHandler
